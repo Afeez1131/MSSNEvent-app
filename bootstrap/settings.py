6450dd90837d106da28c00c89660eff70b1230db
Lawal Afeez <lawalafeez052@gmail.com>
2021-09-13 18:23:04 +0200

    added white noise

--- bootstrap/settings.py
+++ bootstrap/settings.py
@@ -144,6 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(STATIC_ROOT, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 AUTH_USER_MODEL = 'auth_app.CustomUser'
 
