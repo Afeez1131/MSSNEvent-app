dbeae8df14b976af06eca2c0a0c9d1f64ee462cc
Lawal Afeez <lawalafeez052@gmail.com>
2021-09-14 08:16:07 +0200

    debug changed

--- bootstrap/settings.py
+++ bootstrap/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ "event-mssn.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["event-mssn.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
