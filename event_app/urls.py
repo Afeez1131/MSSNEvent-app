45a4e6f7e63ddd8687096e1848c85547aa0a6306
afeez1131 <lawalafeez052@gmail.com>
2022-11-09 08:37:34 +0100

    use ajax for pagination

--- event_app/urls.py
+++ event_app/urls.py
@@ -6,33 +6,22 @@ urlpatterns = [
 
     path('export_csv/<slug>/', views.export_all_attendant, name='export_csv'),
 
-    path('export_csv/<slug>/<day>/',
-         views.export_days_attendant, name='export_day_csv'),
-
+    path('export_csv/<slug>/<day>/', views.export_days_attendant, name='export_day_csv'),
     path('event/', views.event_list, name='event_list'),
     # list of all events
     path('event/<int:year>/', views.event_list_year, name='event_list_year'),
     # list of event by year
     path('event/create/', views.event_create, name='event_create'),
     # create new event
-
-    path('attendant/<slug>/create/',
-         views.create_attendant, name='attendant_create'),
-
-    path('attendant/<slug>/',
-         views.all_attendants, name='event_attendant'),
+    path('attendant/<slug>/create/', views.create_attendant, name='attendant_create'),
+    path('attendant/<slug>/', views.all_attendants, name='event_attendant'),
     path('all-attendants/', views.ajax_all_attendants),
     # return all attendant for an event with the slug passed in the url *args
-    path('attendant/<slug>/<day>/',
-         views.attendant_by_day, name='event_list_day'),
+    path('attendant/<slug>/<day>/', views.attendant_by_day, name='event_list_day'),
     # return the attendant for the day passed with the url *args
-
     path('dashboard/<slug:slug>/', views.dashboard_all, name='dashboard'),
-
     # create a new attendant
-
-    path('dashboard/<slug:slug>/<day>/',
-         views.dashboard, name='dashboard'),
+    path('dashboard/<slug:slug>/<day>/', views.dashboard, name='dashboard'),
     path('create-event-attendants', views.ajax_create_attendants),
-
+    path('paginate/', views.ajax_paginate)
 ]
